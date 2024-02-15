1d43ac7679d9f53c345c7055e90e3c04a96e186a
Surendar Singh <surendarsingh2002@gmail.com>
2024-02-15 13:11:27 +0530

    fixed error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <a hre></a>
       <Head />
       <body>
         <Main />
